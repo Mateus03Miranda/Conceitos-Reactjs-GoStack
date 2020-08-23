af8627a7ece57ac956685b243f89e5b57f2cb24f
Mateus Miranda <mateusm.miranda80@gmail.com>
2020-08-23 17:24:14 -0300

    Finalizando o Desafio e adicionando o README

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center">
+    <img src="https://ik.imagekit.io/d4plefqyqv/RS_OEcD0tpViG.png">
+    Conceito ReactJS
+</h1>
+## 📝️ Sobre
+
+No módulo **Conceitos do ReactJS** foi estudado tópicos como:
+- Conceitos do React e ReactJS
+- Conceitos de SPA e JSX
+- Pilares do React (Componentização, Propriedade e Estado)
+- Funcionamento do Babel e Webpack
+- Funcionamento do Axios
+
+Nesse desafio consiste em criar a interface da aplicação que armazena repositórios. Para isso foi utilizado o Axios para receber os dados do Back-End e apresentá-los em lista no Front-End. Nesse projeto é possível criar e excluir os repositórios. 
+
+---
+## 🛠️ Tecnologias Utilizadas
+- Javascript
+- React
+- Axios
+
+---
+## 💡️ Como baixar
+```bash
+#Abra o seu terminal e entre nos documentos
+
+cd Documents
+
+#Clone o Projeto
+git clone https://github.com/Mateus03Miranda/Conceitos-Reactjs-GoStack.git
+
+#Entre na pasta do projeto
+cd Conceitos-Reactjs-GoStack
+
+#Execute o comando yarn para baixar as dependências
+
+yarn 
+
+#Execute o comando "yarn dev" para iniciar a aplicação
+
+yarn start
+
+#Execute "yarn test" para testar a aplicação
+
+yarn test
+```
--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 import "./styles.css";
+import api from './services/api.js';
 
 function App() {
+  const [repositories, setRepositories]=useState([]);
+  useEffect(()=>{
+    api.get('/repositories').then(response=>{
+      const repository= response.data;
+      setRepositories(repository);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response=await api.post('/repositories',{
+      title:`Desafio React ${Date.now()}`,
+      owner:'Matias',
+      techs:['ReactJS', 'NodeJS']
+    });
+    const repository=response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    const repos= repositories.filter(repository=>repository.id!==id);
+    setRepositories(repos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
+          {repositories.map(repository=>(
+            <>
+                <li key={repository.id}>{repository.title}</li>
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+            </>))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
